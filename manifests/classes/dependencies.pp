d396c7fe2d82eae65b60638330a14b75b58dfb09
Eli Klein <eklein@rallydev.com>
2011-07-28 14:42:16 -0600

    Removed passenger Changed path back to /usr/local/rvm/bin/rvm from /usr/local/bin/rvm

--- manifests/classes/dependencies.pp
+++ manifests/classes/dependencies.pp
@@ -1,6 +1,5 @@
 class rvm::dependencies {
   case $operatingsystem {
-    Ubuntu: { include rvm::dependencies::ubuntu }
     CentOS: { include rvm::dependencies::centos }
   }
 }
--- manifests/classes/passenger-gem.pp
+++
@@ -1,8 +0,0 @@
-class rvm::passenger::gem($ruby_version, $version, $rvm_prefix) {
-  rvm_gem {
-    "passenger":
-      ruby_version => $ruby_version,
-      ensure => $version,
-      rvm_prefix => $rvm_prefix,
-  }
-}
